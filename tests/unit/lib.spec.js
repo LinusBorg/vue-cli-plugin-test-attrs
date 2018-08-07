fc03860b30f712bb9983010174364b874c198d80
Thorsten <t.luenborg@googlemail.com>
2018-08-07 22:32:07 +0200

    make lib unit tests work

--- tests/unit/lib.spec.js
+++ tests/unit/lib.spec.js
@@ -2,8 +2,10 @@ const compiler = require('vue-template-compiler')
 const fs = require('fs')
 const path = require('path')
 const template = fs.readFileSync(
-  path.join(__dirname, 'resources/template.html')
+  path.join(__dirname, 'resources/template.html'),
+  { encoding: 'utf8' }
 )
+
 const generateCompilerModule = require('../../lib')
 
 function generateRender(options) {
@@ -23,17 +25,21 @@ describe('Tests', () => {
       attrs: ['test', 'e2e'],
     })
 
-    // expect(render).toEqual(expent.stringContaining(`'data-test': 'paragraph'`))
+    expect(render).toEqual(expect.stringContaining(`"data-test":"paragraph"`))
+    expect(render).toEqual(expect.stringContaining(`"data-e2e":"div"`))
     expect(render).toMatchSnapshot()
   })
 
   it('renders without test attribute when enabled', () => {
     const render = generateRender({
-      enabled: false,
+      enabled: true,
       attrs: ['test', 'e2e'],
     })
 
-    // expect(render).toEqual(expect.not.stringContaining(`'data-test': 'paragraph'`))
+    expect(render).toEqual(
+      expect.not.stringContaining(`"data-test":"paragraph"`)
+    )
+    expect(render).toEqual(expect.not.stringContaining(`"data-e2e":"div"`))
     expect(render).toMatchSnapshot()
   })
 })
