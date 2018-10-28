40ce6f14bcdf5b0e970818cf05890a7993f6e811
Thorsten <t.luenborg@googlemail.com>
2018-10-28 22:13:45 +0100

    moved /src to /example to avoid confusion

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   lintOnSave: false,
+  chainWebpack: config => {
+    const path = require('path')
+    config.resolve.alias
+      .delete('@')
+      .set('#example', path.resolve(__dirname, './example'))
+      .set('#lib', path.resolve(__dirname, './lib'))
+  },
 }
