24ac575dfe395f4b6c769c2b583fe75a9263b285
Thorsten <t.luenborg@googlemail.com>
2019-03-14 20:24:45 +0100

    refactor: move some logic into smaller functions.

--- index.js
+++ index.js
@@ -5,21 +5,6 @@
 
 const generateCompilerModule = require('./lib/')
 
-const genDefOpts = () => {
-  let enabled
-  if (process.env.VUE_CLI_KEEP_TEST_ATTRS) {
-    enabled = false
-  }
-
-  if (enabled === undefined && process.env.NODE_ENV === 'test') {
-    enabled = false
-  }
-  return {
-    enabled,
-    attrs: ['test'],
-  }
-}
-
 module.exports = (api, projectOptions) => {
   const defaultOptions = genDefOpts()
 
@@ -28,19 +13,10 @@ module.exports = (api, projectOptions) => {
 
   if (options.enabled === false) return
 
-  const vueLoaderCacheConfig = api.genCacheConfig('vue-loader', {
-    'vue-loader': require('vue-loader/package.json').version,
-    /* eslint-disable-next-line node/no-extraneous-require */
-    '@vue/component-compiler-utils': require('@vue/component-compiler-utils/package.json')
-      .version,
-    'vue-template-compiler': require('vue-template-compiler/package.json')
-      .version,
-    testAttrsOptions: JSON.stringify(options),
-  })
-
   api.chainWebpack(config => {
     const rule = config.module.rule('vue')
 
+    const vueLoaderCacheConfig = genVueLoaderCacheConfig()
     rule.use('cache-loader').tap(() => vueLoaderCacheConfig)
 
     rule.use('vue-loader').tap(vueLoaderOptions => {
@@ -58,4 +34,31 @@ module.exports = (api, projectOptions) => {
       }
     })
   })
+
+  function genDefOpts() {
+    let enabled
+    if (process.env.VUE_CLI_KEEP_TEST_ATTRS) {
+      enabled = false
+    }
+
+    if (enabled === undefined && process.env.NODE_ENV === 'test') {
+      enabled = false
+    }
+    return {
+      enabled,
+      attrs: ['test'],
+    }
+  }
+
+  function genVueLoaderCacheConfig() {
+    return api.genCacheConfig('vue-loader', {
+      'vue-loader': require('vue-loader/package.json').version,
+      /* eslint-disable-next-line node/no-extraneous-require */
+      '@vue/component-compiler-utils': require('@vue/component-compiler-utils/package.json')
+        .version,
+      'vue-template-compiler': require('vue-template-compiler/package.json')
+        .version,
+      testAttrsOptions: JSON.stringify(options),
+    })
+  }
 }
