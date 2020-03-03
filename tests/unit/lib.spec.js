b1ab36095da208768da75e2ba53e8eb407642f86
Peter Rietveld <p.rietveld@ridder.nl>
2020-03-03 13:32:07 +0100

    Extend unit test with data bind attribute

--- tests/unit/lib.spec.js
+++ tests/unit/lib.spec.js
@@ -28,7 +28,13 @@ describe('Tests', () => {
     expect(render).toEqual(
       expect.not.stringContaining(`"data-test":"paragraph"`)
     )
+    console.log(render)
+
     expect(render).toEqual(expect.not.stringContaining(`"data-e2e":"div"`))
+    expect(render).toEqual(expect.not.stringContaining(`"data-test":div-bound`))
+    expect(render).toEqual(
+      expect.not.stringContaining(`"data-test":div-bound-shorthand`)
+    )
     expect(render).toMatchSnapshot()
   })
 })
